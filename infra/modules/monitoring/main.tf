2dcc56f78f88ced3db3be26f17420d94e34a7d84
kennethchinedu <anamsken60@gmail.com>
2026-02-21 13:29:56 +0100

    setting up promethus and istio

--- infra/modules/monitoring/main.tf
+++ infra/modules/monitoring/main.tf
@@ -1,35 +1,35 @@
-#Prometheus installation
-
-resource "helm_release" "prometheus_server" {
+resource "helm_release" "prometheus" {
   name             = "prometheus"
   repository       = "https://prometheus-community.github.io/helm-charts"
-  chart            = "kube-prometheus-stack"
+  chart            = "prometheus"
   namespace        = "monitoring"
   create_namespace = true
-
-  # Optional: override values
-  values = [
-    file("values.yaml")
-  ]
 
   
-  timeout = 600
-#   atomic  = true
-}
+  version = "25.21.0"
+  atomic          = true
+  values = [<<EOF
+
+server:
+  replicaCount: 1
 
 
-resource "helm_release" "grafana" {
-  name             = "grafana"
-  repository       = "https://grafana.github.io/helm-charts"
-  chart            = "grafana"
-  namespace        = "monitoring"
-  create_namespace = true
+alertmanager:
+  enabled: true
 
-  values = [
-    file("values.yaml")
+EOF
   ]
-
-#   atomic  = true
-  timeout = 300
 }
 
+resource "helm_release" "blackbox_exporter" {
+  name             = "blackbox-exporter"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "prometheus-blackbox-exporter"
+  namespace        = "monitoring"
+  create_namespace = false
+  version          = "0.25.0"   
+
+
+  atomic          = true
+  cleanup_on_fail = true
+}
